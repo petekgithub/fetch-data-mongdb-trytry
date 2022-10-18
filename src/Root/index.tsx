37f137cd4fae88b8803762a3e17117e7a1ceb3a7
petekgithub <petekshamitbeyli@gmail.com>
2022-10-18 23:50:23 +0300

    eslint-prettier-setup-and-firstcommit

---
+++ src/Root/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+
+const Root: React.FC = () => {
+  return (
+    <Routes>
+      <Route>
+        <h1>HEY!</h1>
+      </Route>
+    </Routes>
+  )
+}
+
+export default Root;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+//import "./Core/styles/main.scss";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div id="modalContainer" />
+    <h1>HEY!</h1>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
